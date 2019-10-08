42e5488bfc1eaebb1481831827901d0f2782de5b
FWolfe <noreply@tekagis.ca>
2019-10-08 11:38:24 -0700

    intial commit

---
+++ rff/interface/container.lua
@@ -0,0 +1,6 @@
+--[[- placeholder container handling functions, to be overwritten by the application or bridge
+
+]]
+local Container = {}
+
+return Container
---
+++ rff/interface/player.lua
@@ -0,0 +1,6 @@
+--[[- placeholder player handling functions, to be overwritten by the application or bridge
+
+]]
+local Player = {}
+
+return Player
