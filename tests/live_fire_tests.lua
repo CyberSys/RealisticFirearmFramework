790cce875918b77e5813059a385c4c4b26717077
FWolfe <noreply@tekagis.ca>
2019-10-10 12:53:54 -0700

    cleaned redundant code

--- tests/live_fire_tests.lua
+++ tests/live_fire_tests.lua
@@ -14,9 +14,11 @@ Tests.reset()
 local firearm_data
 
 Tests.run("Ammo Registration", function()
+
     local AmmoGroup = Ammo.AmmoGroup
     local AmmoType = Ammo.AmmoType
     local Flags = Ammo.Flags
+
     assert(AmmoGroup:new("AmmoGroup_357Magnum"), 
         "Failed to register ammo group", true)
     assert(AmmoType:new("Ammo_357Magnum_HP", {
@@ -29,9 +31,9 @@ end)
 
 
 Tests.run("Firearm Registration", function()
+
     local FirearmGroup = Firearm.FirearmGroup
     local FirearmType = Firearm.FirearmType
-
 
     assert(Firearm.FirearmType:new("Colt_Python", {
         weight = 1,
@@ -66,6 +68,7 @@ Tests.run("Spawning", function()
 end)
 
 Tests.run("Operation", function()
+
     Tests.log("Engaging safety...")
     State.setSafe(firearm_data, true)
     assert(not Actions.willFire(firearm_data, nil, nil), "willFire returned true, safety check failed.")
@@ -89,64 +92,87 @@ Tests.run("Operation", function()
 end)
 
 Tests.run("Firing", function()
+    
+    local function assertCapacity(v)
+        assert(firearm_data.current_capacity == v, "Ammo count is wrong. Should be ".. v ..", is at ".. firearm_data.current_capacity)
+    end
+    local function assertPosition(v)
+        assert(firearm_data.cylinder_position == v, "Cylinder in wrong position. Should be "..v..", is at ".. firearm_data.cylinder_position)
+    end
+    local function assertWillFire(expected)
+        local result = Actions.willFire(firearm_data, nil, nil)
+        assert(result == expected, "willFire returned ".. tostring(result)) 
+    end 
+
+    local function assertPreFireShot(expected)
+        local result = Actions.preFireShot(firearm_data, nil, nil, true)
+        assert(result == expected, "preFireShot returned ".. tostring(result)) 
+    end
+
+    local function assertPostFireShot(expected)
+        local result = Actions.postFireShot(firearm_data, nil, nil, true)
+        assert(result == expected, "postFireShot returned ".. tostring(result)) 
+    end
+
+
     Tests.log("Recocking and testing Single-Action shot...")
     Actions.cockHammer(firearm_data, nil, nil, false)
     assert(State.isCocked(firearm_data), "Hammer failed to cock.")
-    assert(firearm_data.cylinder_position == 3, "Cylinder in wrong position. Should be 3, is at ".. firearm_data.cylinder_position)
-    assert(Actions.willFire(firearm_data, nil, nil), "willFire returned false")
-    assert(Actions.preFireShot(firearm_data, nil, nil, true))
+    assertPosition(3)
+    assertWillFire(true)
+    assertPreFireShot(true)
     
     Tests.log("BANG!")
-    assert(Actions.postFireShot(firearm_data, nil, nil, true))
-    assert(firearm_data.cylinder_position == 3, "Cylinder in wrong position. Should be 3, is at ".. firearm_data.cylinder_position)
-    assert(firearm_data.current_capacity == 5, "Ammo count is wrong. Should be 5, is at ".. firearm_data.current_capacity)
+    assertPostFireShot(true)
+    assertPosition(3)
+    assertCapacity(5)
     assert(firearm_data.magazine_data[3] == "Case_357Magnum", "No empty shell casing detected at cylinder position")
     -- TODO: functions for checking total empty case count, and isCase()
     Tests.log("Fire successful. Empty casing detected in cylinder.")
     
     Tests.log("Testing Double-Action shot")
     assert(not State.isCocked(firearm_data), "Hammer is already cocked.")
-    assert(Actions.willFire(firearm_data, nil, nil), "willFire returned false")
-    assert(Actions.preFireShot(firearm_data, nil, nil, true))
-    assert(firearm_data.cylinder_position == 4, "Cylinder in wrong position. Should be 4, is at ".. firearm_data.cylinder_position)
+    assertWillFire(true)
+    assertPreFireShot(true)
+    assertPosition(4)
     
     Tests.log("BANG!")
-    assert(Actions.postFireShot(firearm_data, nil, nil, true))
-    assert(firearm_data.cylinder_position == 4, "Cylinder in wrong position. Should be 4, is at ".. firearm_data.cylinder_position)
-    assert(firearm_data.current_capacity == 4, "Ammo count is wrong. Should be 4, is at ".. firearm_data.current_capacity)
+    assertPostFireShot(true)
+    assertPosition(4)
+    assertCapacity(4)
     assert(firearm_data.magazine_data[3] == "Case_357Magnum", "No empty shell casing detected at cylinder position")
     
     Tests.log("Emptying cylinder...")
-    assert(Actions.willFire(firearm_data, nil, nil), "willFire returned false")
-    assert(Actions.preFireShot(firearm_data, nil, nil, true))
-    assert(firearm_data.cylinder_position == 5, "Cylinder in wrong position. Should be 5, is at ".. firearm_data.cylinder_position)
+    assertWillFire(true)
+    assertPreFireShot(true)
+    assertPosition(5)
     Tests.log("BANG!")
-    assert(Actions.postFireShot(firearm_data, nil, nil, true))
-    assert(firearm_data.current_capacity == 3, "Ammo count is wrong. Should be 3, is at ".. firearm_data.current_capacity)
-    assert(Actions.willFire(firearm_data, nil, nil), "willFire returned false")
-    assert(Actions.preFireShot(firearm_data, nil, nil, true))
-    assert(firearm_data.cylinder_position == 6, "Cylinder in wrong position. Should be 6, is at ".. firearm_data.cylinder_position)
+    assertPostFireShot(true)
+    assertCapacity(3)
+    assertWillFire(true)
+    assertPreFireShot(true)
+    assertPosition(6)
     Tests.log("BANG!")
-    assert(Actions.postFireShot(firearm_data, nil, nil, true))
-    assert(firearm_data.current_capacity == 2, "Ammo count is wrong. Should be 2, is at ".. firearm_data.current_capacity)
-    assert(Actions.willFire(firearm_data, nil, nil), "willFire returned false")
-    assert(Actions.preFireShot(firearm_data, nil, nil, true))
-    assert(firearm_data.cylinder_position == 1, "Cylinder in wrong position. Should be 1, is at ".. firearm_data.cylinder_position)
+    assertPostFireShot(true)
+    assertCapacity(2)
+    assertWillFire(true)
+    assertPreFireShot(true)
+    assertPosition(1)
     Tests.log("BANG!")
-    assert(Actions.postFireShot(firearm_data, nil, nil, true))
-    assert(firearm_data.current_capacity == 1, "Ammo count is wrong. Should be 1, is at ".. firearm_data.current_capacity)
+    assertPostFireShot(true)
+    assertCapacity(1)
 
-    assert(Actions.willFire(firearm_data, nil, nil), "willFire returned false")
-    assert(Actions.preFireShot(firearm_data, nil, nil, true))
-    assert(firearm_data.cylinder_position == 2, "Cylinder in wrong position. Should be 2, is at ".. firearm_data.cylinder_position)
+    assertWillFire(true)
+    assertPreFireShot(true)
+    assertPosition(2)
     Tests.log("BANG!")
-    assert(Actions.postFireShot(firearm_data, nil, nil, true))
-    assert(firearm_data.current_capacity == 0, "Ammo count is wrong. Should be 0, is at ".. firearm_data.current_capacity)
+    assertPostFireShot(true)
+    assertCapacity(0)
     Tests.log("Cylinder Empty")
     for i=1, 6 do
         assert(firearm_data.magazine_data[i] == "Case_357Magnum", "No empty shell casing detected at cylinder position ".. i)
     end
-    assert(not Actions.willFire(firearm_data, nil, nil), "willFire returned true")
+    assertWillFire(false)
 end)
 
 
