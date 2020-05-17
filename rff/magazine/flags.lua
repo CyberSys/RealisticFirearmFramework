aa708550ad78b7866d20cab521f08f8144e76169
FWolfe <noreply@tekagis.ca>
2020-05-17 07:59:21 -0700

    adjust magazine flags, new flags for internal mags

--- rff/magazine/flags.lua
+++ rff/magazine/flags.lua
@@ -1,13 +1,20 @@
-local Flags = {}
+local Flags = { }
 
-Flags.BOX = 1
-Flags.TUBE = 2
-Flags.DRUM = 4
-Flags.CASKET = 8
-Flags.STEEL = 16
-Flags.POLYMER = 32
-Flags.PEEKSTRIP = 64
-Flags.BULK = 128
-Flags.MATCHGRADE = 256
+Flags.INTERNAL = 1
+Flags.LOADGATE = 2 -- internal magazines. has a loading gate primarly for (tubes, old revolvers) 
+Flags.FIXED = 4 -- internal mags. loading gate must be used. (cylnder doesnt swing out, tube doesnt slide) 
+Flags.ROTARY = 8
+Flags.TUBE = 16
+--Flags.TUBE = 32
+
+Flags.BOX = 64
+Flags.DRUM = 128
+Flags.CASKET = 256
+
+Flags.STEEL = 512
+Flags.POLYMER = 1024
+Flags.PEEKSTRIP = 2048
+Flags.BULK = 4096
+Flags.MATCHGRADE = 8192
 
 return Flags
